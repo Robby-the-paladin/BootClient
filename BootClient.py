557e593601357b7a1b4db4bdfba5ef445efb2b1b
Robby-the-paladin <71353456+Robby-the-paladin@users.noreply.github.com>
2022-03-24 12:06:12 +0300

    Pyhon commit

--- BootClient.py
+++ BootClient.py
@@ -1,17 +1,37 @@
 from git import Repo
+from git import RemoteProgress
+from PyQt6 import QtWidgets
+
+import sys
 
 PATH_OF_GIT_REPO = r'C:\\Users\\mea25\\Desktop\\BootClient\\BootClient\\.git'  # make sure .git folder is properly configured
 COMMIT_MESSAGE = 'Pyhon commit'
+
+class ProgressPrinter(RemoteProgress):
+    def update(self,
+               op_code,
+               cur_count,
+               max_count=None,
+               message=''):
+        print("progress\n")
 
 def git_push():
     try:
         repo = Repo(PATH_OF_GIT_REPO)
         repo.git.add(update=True)
-        repo.git.add("BootClient.py")
+        repo.git.add("env")
         repo.index.commit(COMMIT_MESSAGE)
         origin = repo.remote(name='origin')
-        origin.push()
+        origin.push(progress=ProgressPrinter())
     except:
         print('Some error occured while pushing the code')    
+
+def git_pull():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        origin = repo.remote(name='origin')
+        origin.pull(progress=ProgressPrinter())
+    except:
+        print('Some error occured while pulling the code')
 
 git_push()
